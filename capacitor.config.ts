3d0043a80fa518383d5e74b6fca229f7c81791e1
RoxZkiL <mttrox@gmail.com>
2026-02-19 18:39:38 -0300

    fix: eliminar elementos no utilizados de capacitor en config y app.component

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,17 +4,6 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'book-app',
   webDir: 'www',
-
-  plugins: {
-    CapacitorSQLite: {
-      iosIsEncryption: false,
-      iosKeychainPrefix: 'book-app',
-      iosDatabaseLocation: 'Library/CapacitorDatabase',
-      androidIsEncryption: false,
-      electronIsEncryption: false,
-      electronWindowsLocation: 'C:\\ProgramData\\CapacitorDatabases',
-    },
-  },
 };
 
 export default config;
